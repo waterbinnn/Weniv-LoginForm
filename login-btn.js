7a3ee08ecdb36d2629fccc9f6772f4cbae0dd6f1
Subin <ewaterbin@naver.com>
2022-04-18 13:49:01 +0900

    css 수정

--- login-btn.js
+++ login-btn.js
@@ -2,10 +2,10 @@ let checkimg = document.querySelector("#check");
 
 checkimg.onclick = function () {
   let checksrc = checkimg.getAttribute('src');
-  if (checksrc === "./img/icon_check_empty.png"){
-    check.setAttribute('src',"./img/icon_check.png");
+  if (checksrc === "img/icon_check_empty.png"){
+    check.setAttribute('src',"img/icon_check.png");
   }
   else {
-    check.setAttribute('src', "./img/icon_check_empty.png")
+    check.setAttribute('src', "img/icon_check_empty.png")
   }
 }
